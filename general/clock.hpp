51fc6e5ec16c6ad9f6ab38c04d3f4d668890ea74
jee-eun.lee <jee-eun.lee@dexterity.ai>
2024-08-22 15:47:38 -0700

    feat: add timer print

--- general/clock.hpp
+++ general/clock.hpp
@@ -12,8 +12,24 @@ class Clock {
         double stop() {
             end_time_ = std::chrono::high_resolution_clock::now();
             duration_ = std::chrono::duration_cast<std::chrono::microseconds>(end_time_-ini_time_);
-            return double(duration_.count())*1e-3;
+            return double(duration_.count());
         }
+        void printElapsedSec(const std::string_view message){
+            std::cout << message << stop()*1e-6 << " sec " << std::endl;
+            start();
+        }
+
+        void printElapsedMiliSec(const std::string_view message){
+            std::cout << message << stop()*1e-3 << " msec " << std::endl;
+            start();
+        }
+
+        void printElapsedMicroSec(const std::string_view message){
+            std::cout << message << stop() << " µsec " << std::endl;
+            start();
+        }
+
+
 
     private:
         std::chrono::microseconds duration_;
